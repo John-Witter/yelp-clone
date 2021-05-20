b5d910115555d0cc665d5ea4dfc5fe4ac0212908
John-Witter <mrsolginski@gmail.com>
2021-05-20 18:53:13 -0400

    finished Phase 5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,6 +7,20 @@ const { User } = require('../../db/models')
 
 const router = express.Router()
 
+// Restore session user
+router.get(
+    '/', 
+    restoreUser,
+    (req, res) => {
+        const { user } = req
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            })
+        } else return res.json({})
+    }
+)
+
 // Log in
 router.post(
     '/',
