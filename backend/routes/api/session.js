bc9d4a8bf020f35d4e1839b36e3b97782e5e0859
John-Witter <mrsolginski@gmail.com>
2021-05-20 18:38:23 -0400

    finished Phase 4: User Auth Routes: User Logout API Route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -31,4 +31,13 @@ router.post(
     })
 )
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token')
+        return res.json({ message: 'success'})
+    }
+)
+
 module.exports = router
