42bf351aed8bb5ef48252a4f1eddf35524c87fc6
John-Witter <mrsolginski@gmail.com>
2021-05-20 14:14:58 -0400

    finished Phase 1: API Routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 // backend/routes/index.js
 const express = require('express')
 const router = express.Router()
+const apiRouter = require('./api')
+
+// all the URLs of the routes in the api router will be prefixed with /api
+router.use('/api', apiRouter)
 
 router.get('/hello/world', function(req,res) {
     res.cookie("XSRF-TOKEN", req.csrfToken())
