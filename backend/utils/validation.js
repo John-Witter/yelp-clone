5e695c8d69bf84b5bc55c777bf0f0b1301249fff
John-Witter <mrsolginski@gmail.com>
2021-05-21 10:31:33 -0400

    finished Phase 5: validationResult

---
+++ backend/utils/validation.js
@@ -0,0 +1,29 @@
+// backend/utils/validation.js
+const { validationResult } = require('express-validator')
+
+//middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+// https://express-validator.github.io/docs/
+// https://www.npmjs.com/package/express-validator
+
+const handleValidationErrors = (req, _res, next) => {
+    const validationErrors = validationResult(req)
+
+    if (!validationErrors.isEmpty()) {
+        const errors = validationErrors
+            .array()
+            .map(e => `${e.msg}`)
+
+        const err = Error('Bad request.')
+        err.errors = errors
+        err.status = 400
+        err.title = 'Bad request.'
+        next(err)
+    }
+
+    next()
+}
+
+module.exports = {
+    handleValidationErrors
+}
