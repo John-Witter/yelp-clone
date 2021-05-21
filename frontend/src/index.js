d9f35a09fc1081a164601a500a1e642e133ae564
John-Witter <mrsolginski@gmail.com>
2021-05-21 13:10:52 -0400

    finished Authenticate Me - Frontend Phase 0: Set Up

---
+++ frontend/src/index.js
@@ -0,0 +1,40 @@
+// frontend/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import App from './App';
+import configureStore from "./store";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+const store = configureStore()
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
