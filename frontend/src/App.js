ea81d901c52546c016d1037cfec97c70387d7155
John-Witter <mrsolginski@gmail.com>
2021-05-21 16:48:26 -0400

    finished Phase 2 SignupFormPage Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from 'react-router-dom'
 import LoginFormPage from './components/LoginFormPage'
+import SignupFormPage from './components/SignupFormPage'
 import * as sessionActions from './store/session'
 
 function App() {
@@ -18,6 +19,10 @@ function App() {
     <Switch>
       <Route path="/login">
         <LoginFormPage />
+      </Route>
+
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
     </Switch>
   );
