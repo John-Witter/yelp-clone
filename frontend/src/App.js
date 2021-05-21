a8bcab40839bb843072ecf8273031eb73a54746e
John-Witter <mrsolginski@gmail.com>
2021-05-21 17:48:40 -0400

    finished Phase 3: Navigation Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from 'react-router-dom'
 import LoginFormPage from './components/LoginFormPage'
 import SignupFormPage from './components/SignupFormPage'
+import Navigation from './components/Navigation'
 import * as sessionActions from './store/session'
 
 function App() {
@@ -16,15 +17,19 @@ function App() {
   }, [dispatch])
 
   return isLoaded && (
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-
-      <Route path="/signup">
-        <SignupFormPage />
-      </Route>
-    </Switch>
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
+    </>
   );
 }
 
