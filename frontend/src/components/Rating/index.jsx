9afa58f0eb43358f4549d8ce1fb1237b19874b9c
John-Witter <mrsolginski@gmail.com>
2021-05-28 09:17:55 -0400

    styled /businesses/:id

--- frontend/src/components/Rating/index.jsx
+++ frontend/src/components/Rating/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { csrfFetch } from "../../store/csrf";
+import './Rating.css'
 
 const Rating = ({ id }) => {
     const [rating, setRating] = useState(1)
@@ -25,11 +26,12 @@ const Rating = ({ id }) => {
 
     return (
         <div className="rating-container">
-            <input type="radio" name="ratingValue" value='1' id='1' onClick={(e) => handleClick(e)}/>
-            <input type="radio" name="ratingValue" value='2' id='2' onClick={(e) => handleClick(e)} />
-            <input type="radio" name="ratingValue" value='3' id='3' onClick={(e) => handleClick(e)} />
-            <input type="radio" name="ratingValue" value='4' id='4' onClick={(e) => handleClick(e)} />
-            <input type="radio" name="ratingValue" value='5' id='5' onClick={(e) => handleClick(e)} />
+            <label htmlFor="rating" className='rating-label'>Rating</label>
+            <input id='rating' className="rating" type="radio" name="ratingValue" value='1' id='1' onClick={(e) => handleClick(e)}/>
+            <input id='rating' className="rating" type="radio" name="ratingValue" value='2' id='2' onClick={(e) => handleClick(e)} />
+            <input id='rating' className="rating" type="radio" name="ratingValue" value='3' id='3' onClick={(e) => handleClick(e)} />
+            <input id='rating' className="rating" type="radio" name="ratingValue" value='4' id='4' onClick={(e) => handleClick(e)} />
+            <input id='rating' className="rating" type="radio" name="ratingValue" value='5' id='5' onClick={(e) => handleClick(e)} />
         </div>
     )
 }
--- frontend/src/components/ShowSingleRestaurant/index.jsx
+++ frontend/src/components/ShowSingleRestaurant/index.jsx
@@ -4,15 +4,16 @@ import { useParams } from "react-router-dom";
 import { getBusinessById } from '../../store/yelp-api'
 import Rating from "../Rating";
 import Review from "../Review";
+import './ShowSingleRestaurant.css'
 
 const ShowSingleRestaurant = () => {
     const { id } = useParams()
     const dispatch = useDispatch()
     const [business, setBusiness] = useState([])
     const user = useSelector(state => state.session.user)
-    
+
     useEffect(() => {
-        const getCurrentBusiness = async() => {
+        const getCurrentBusiness = async () => {
             const currentBusiness = await dispatch(getBusinessById(id))
             console.log('currentBusiness', currentBusiness)
             setBusiness(currentBusiness.data)
@@ -20,17 +21,26 @@ const ShowSingleRestaurant = () => {
         getCurrentBusiness()
     }, [dispatch, id])
     return (
-        <div>
-            
+        <div className='business-parent'>
+            {/*             
+                <img src={business.image_url} alt={business.name}
+                    className='business-photo'
+                />
+                <div className='business-name'>{business.name}</div>
+                <div className='business-price'>Price: {business.price}</div>
+                <div className='business-rating'>Rating: {business.rating}</div> */}
+
+            <div className='business-frame' key={business.id}>
                 <img src={business.image_url} alt={business.name}
                     className='business-photo'
                 />
                 <div className='business-name'>{business.name}</div>
                 <div className='business-price'>Price: {business.price}</div>
                 <div className='business-rating'>Rating: {business.rating}</div>
-                {user && <Review id={id}/>}
-                {user && <Rating id={id} />}
-            
+            </div>
+            {user && <Review id={id} />}
+            {user && <Rating id={id} />}
+
         </div>
 
     )
