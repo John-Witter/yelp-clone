b001bcfcd3e303b7c76c0a3ab9a50cac7484ac9e
John-Witter <mrsolginski@gmail.com>
2021-07-27 15:49:26 -0400

    centerred  tag-line and search-term-result

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
     useEffect(() => {
         dispatch(getBusinessByName(searchTerm, location))
     }, [dispatch])
-    
+
     const handleBusinessClick = (id) => {
         history.push(`/businesses/${id}`)
     }
@@ -23,26 +23,26 @@ const HomePage = () => {
 
         <div className='homepage-body'>
             <div className="intro-text">
-                <h1>Find businesses anywhere!</h1>
+                <h1 className='tag-line'>Find businesses anywhere!</h1>
+                <p className='search-term-result'>The following results are based on the most recent search of "{searchTerm}" near "{location}"</p>
             </div>
             {searchTerm && location && businessesObj &&
-            <div>
-                <p className='search-term-result'>The following results are based on the most recent search of "{searchTerm}" near "{location}"</p>
-                <div className='business-parent'>
-                    {businessesObj?.map((business, idx) => (
-                        <div className='business-frame' key={business.id}
-                            onClick={() => handleBusinessClick(business.id)}
-                        >
-                            <img src={business.image_url} alt={business.name}
-                                className='business-photo'
-                            />
-                            <div className='business-name'>{business.name}</div>
-                            <div className='business-price'>Price: {business.price}</div>
-                            <div className='business-rating'>Rating: {business.rating}</div>
-                        </div>
-                    ))}
-                </div>
-            </div>}
+                <div>
+                    <div className='business-parent'>
+                        {businessesObj?.map((business, idx) => (
+                            <div className='business-frame' key={business.id}
+                                onClick={() => handleBusinessClick(business.id)}
+                            >
+                                <img src={business.image_url} alt={business.name}
+                                    className='business-photo'
+                                />
+                                <div className='business-name'>{business.name}</div>
+                                <div className='business-price'>Price: {business.price}</div>
+                                <div className='business-rating'>Rating: {business.rating}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div>}
         </div>
     )
 }
