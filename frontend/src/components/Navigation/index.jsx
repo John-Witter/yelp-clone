68b0c9191392c1fd978e31be27b558d69c1c5f03
John-Witter <mrsolginski@gmail.com>
2021-07-28 10:46:25 -0400

    demo btn && navbar layout

--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -8,6 +8,15 @@ const Navigation = ({ isLoaded }) => {
     const dispatch = useDispatch()
     const sessionUser = useSelector(state => state.session.user)
 
+    const demoUser = () => {
+        let credential = 'demo@user.io'
+        let password = 'password'
+        dispatch(sessionActions.login({ credential, password }))
+            .catch(async (res) => {
+                await res.json()
+            })
+    }
+
     const logout = (e) => {
         dispatch(sessionActions.logout());
     };
@@ -16,15 +25,19 @@ const Navigation = ({ isLoaded }) => {
         <ul className='nav-body'>
             <NavLink exact to='/' className='title'>
                 <div className='title'>Yelp Jr.</div>
-            </NavLink>            
+            </NavLink>
             <RestaurantSearch className='search' />
             {!sessionUser && <div className='link-parent'>
                 <NavLink className='login' to='/login'>Log In</NavLink>
                 <NavLink className='signup' to='/signup'>Sign Up</NavLink>
+                <div className='demo' onClick={demoUser}>
+                    Demo
+                </div>
             </div>}
             {sessionUser && (
                 <div className='logout' onClick={logout}>Log Out</div>
             )}
+            {/* {!sessionUser && } */}
         </ul>
 
     )
