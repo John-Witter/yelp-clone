3c4190729a074468c8a05a01ac4106828743a3b5
John-Witter <mrsolginski@gmail.com>
2021-08-01 09:20:14 -0400

    homepage user location works!!!!!!

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -2,33 +2,36 @@ import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getBusinessByName } from "../../store/yelp-api";
-import { getUserLocation } from "../../store/userLocation";
+// import { getUserLocation } from "../../store/userLocation";
 import './HomePage.css'
 
 const HomePage = () => {
-    const userLocation = useSelector(state => state.userLocation)
+    // const userLocation = useSelector(state => state.userLocation)
     const dispatch = useDispatch()
     // const searchTerm = 'restaurants'
     // const location = 'manhattan'
     const [searchTerm, setSearchTerm] = useState('restaurants')
-    const [location, setLocation] = useState()
+    const [location, setLocation] = useState('')
     const businessesObj = useSelector(state => Object.values(state.yelpAPI))
     const history = useHistory()
+
+
+    useEffect(() => {
+        const getUserLocation = async () => {
+            const res = await fetch('https://ipapi.co/json/')
+            if (res.ok) {
+                const data = await res.json()
+                setLocation(data.city)
+            }
+        }
+        getUserLocation()
+    }, [dispatch])
     
     useEffect(() => {
-        dispatch(getUserLocation())
-        userLocation.city && setLocation(userLocation.city)
-    }, [dispatch])
-
-    useEffect(() => {        
-        if (userLocation.city) {
-            console.log('!!!!!!userLocation', userLocation)
-            setLocation(userLocation.city)
+        if (location) {
             dispatch(getBusinessByName(searchTerm, location))
         }
-    }, [userLocation])
-    
-    
+    }, [location])
 
     const handleBusinessClick = (id) => {
         history.push(`/businesses/${id}`)
@@ -37,11 +40,11 @@ const HomePage = () => {
     return (
 
         <div className='homepage-body'>
-            {userLocation && <div className="intro-text">
+            {location && <div className="intro-text">
                 <h1 className='tag-line'>Find businesses anywhere!</h1>
                 <p className='search-term-result'>The following results are based on the most recent search of "{searchTerm}" near "{location}"</p>
             </div>}
-            {userLocation && searchTerm && location && businessesObj &&
+            {location && searchTerm && location && businessesObj &&
                 <div>
                     <div className='business-parent'>
                         {businessesObj?.map((business, idx) => (
