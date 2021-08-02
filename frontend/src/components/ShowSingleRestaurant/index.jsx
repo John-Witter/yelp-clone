9ac2539a8723e2c90777c89b7433dfd5b356653e
John-Witter <mrsolginski@gmail.com>
2021-08-02 11:58:54 -0400

    Stars on user-ratings

--- frontend/src/components/ShowSingleRestaurant/index.jsx
+++ frontend/src/components/ShowSingleRestaurant/index.jsx
@@ -91,7 +91,8 @@ const ShowSingleRestaurant = () => {
                                     <img className='yelp-user-photo user-photo' src='https://images.unsplash.com/photo-1547354142-526457358bb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHNpbGhvdWV0dGV8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='profile pic' />{user && review.User.username === user.username ? `Your Review` : review.User.username} <span className='review-time-created'>{review.createdAt.split('T')[0]}</span>
                                 </div>
                                 <div className="user-ratings">
-                                    Rating: {userRatings[idx] && userRatings[idx].rating}
+                                    {/* Rating: {userRatings[idx] && userRatings[idx].rating} */}
+                                    <Stars rating={userRatings[idx].rating} size='small' />
                                 </div>
                                 <div className="user-reviews">
                                     {review.reviewText && review.reviewText}
